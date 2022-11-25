29bed5215856cafea687951789b78f6f84bc5674
Patrick Hochstenbach <Patrick.Hochstenbach@UGent.be>
2022-11-25 07:56:31 +0100

    Adding pages

--- gh-pages.js
+++ gh-pages.js
@@ -4,7 +4,7 @@ ghpages.publish(
 	'public', // path to public directory
 	{
 		branch: 'gh-pages',
-		repo: 'git@github.com:phochste/AcmeBase.git', // Update to point to your repository
+		repo: 'git@github.com:phochste/SolidViewer.git', // Update to point to your repository
 		user: {
 			name: 'Patrick Hochstenbach', // update to use your name
 			email: 'patrick.hochstenbach@ugent.be' // Update to use your email
